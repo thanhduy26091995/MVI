a0b04bc2ededf832bdb1f60664ef6b51093069f7
Dennis <buivuthanhduy@gmail.com>
2025-10-22 00:04:44 +0700

    Chore: Testing git account

--- app/src/main/java/com/densitech/mvi/ui/theme/Color.kt
+++ app/src/main/java/com/densitech/mvi/ui/theme/Color.kt
@@ -9,3 +9,30 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryColor = Color(0xFF6A3EA1)
+val PrimaryDarkColor = Color(0xFF3A2258)
+val PrimaryLightColor = Color(0xFFEFE9F7)
+val PrimaryBackgroundColor = Color(0xFFFAF8FC)
+
+val SecondaryColor = Color(0xFF60D889)
+val SecondaryDarkColor = Color(0xFF1F7F40)
+val SecondaryLightColor = Color(0xFFDAF6E4)
+
+val NeutralBlackColor = Color(0xFF180E25)
+val NeutralDarkGreyColor = Color(0xFF827D89)
+val NeutralBaseGreyColor = Color(0xFFB3ADB8)
+val NeutralLightGreyColor = Color(0xFFEFEEF0)
+val NeutralWhiteColor = Color(0xFFFFFFFF)
+
+val SuccessBaseColor = Color(0xFF60D889)
+val SuccessLightColor = Color(0xFFDAF6E4)
+val SuccessDarkColor = Color(0xFF1F7F40)
+
+val ErrorBaseColor = Color(0xFFCE3A54)
+val ErrorLightColor = Color(0xFFF7DEE3)
+val ErrorDarkColor = Color(0xFF5A1623)
+
+val WarningBaseColor = Color(0xFFF8C715)
+val WarningLightColor = Color(0xFFFDEBAB)
+val WarningDarkColor = Color(0xFF725A03)
--- app/src/main/java/com/densitech/mvi/ui/theme/Theme.kt
+++ app/src/main/java/com/densitech/mvi/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.densitech.mvi.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,29 +8,68 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = PrimaryColor,
     onPrimary = Color.White,
-    onSecondary = Color.White,
+    primaryContainer = PrimaryLightColor,
+    onPrimaryContainer = PrimaryDarkColor,
+
+    secondary = SecondaryColor,
+    onSecondary = Color.Black,
+    secondaryContainer = SecondaryLightColor,
+    onSecondaryContainer = SecondaryDarkColor,
+
+    background = PrimaryBackgroundColor,
+    onBackground = NeutralBlackColor,
+
+    surface = NeutralWhiteColor,
+    onSurface = NeutralBlackColor,
+
+    error = ErrorBaseColor,
+    onError = Color.White,
+    errorContainer = ErrorLightColor,
+    onErrorContainer = ErrorDarkColor,
+
+    tertiary = SuccessBaseColor,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiaryContainer = SuccessLightColor,
+    onTertiaryContainer = SuccessDarkColor,
+    outline = NeutralBaseGreyColor
 )
+
+private val DarkColors = darkColorScheme(
+    primary = PrimaryLightColor,
+    onPrimary = PrimaryDarkColor,
+    primaryContainer = PrimaryDarkColor,
+    onPrimaryContainer = PrimaryLightColor,
+
+    secondary = SecondaryLightColor,
+    onSecondary = SecondaryDarkColor,
+    secondaryContainer = SecondaryDarkColor,
+    onSecondaryContainer = SecondaryLightColor,
+
+    background = Color(0xFF121212),
+    onBackground = NeutralWhiteColor,
+
+    surface = Color(0xFF1E1E1E),
+    onSurface = NeutralWhiteColor,
+
+    error = ErrorBaseColor,
+    onError = Color.White,
+    errorContainer = ErrorDarkColor,
+    onErrorContainer = ErrorLightColor,
+
+    tertiary = SuccessLightColor,
+    onTertiary = SuccessDarkColor,
+    tertiaryContainer = SuccessDarkColor,
+    onTertiaryContainer = SuccessLightColor,
+
+    outline = NeutralDarkGreyColor
+)
+
 
 @Composable
 fun MVITheme(
@@ -46,7 +84,7 @@ fun MVITheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
+        darkTheme -> DarkColors
         else -> LightColorScheme
     }
 
